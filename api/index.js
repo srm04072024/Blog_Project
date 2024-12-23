e6cc4c69184b1a8a1a562b2eb77b0cea6a15e1d2
srm04072024 <s.r.m.20.3.2018@gmail.com>
2024-12-23 18:54:50 +0530

    feat:Database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const path = 3000;
@@ -6,6 +7,17 @@ const path = 3000;
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://srm2032018:blog2024@blog-2024.8n59t.mongodb.net/?retryWrites=true&w=majority&appName=Blog-2024"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(path, () => {
   console.log(`Server is running on http://localhost:${path}`);
