f8765242aefd50a33fa54d69bc4c7e85d50be2fb
srm04072024 <s.r.m.20.3.2018@gmail.com>
2025-01-01 17:08:52 +0530

    Add custom prfile update functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,20 +1,123 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Button, TextInput } from "flowbite-react";
+import { Button, TextInput, Alert } from "flowbite-react";
+import { useState, useRef, useEffect } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import axios from "axios";
 
 function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const maxSize = 5 * 1024 * 1024;
+    if (!file.type.startsWith("image/")) {
+      setImageFileUploadError("Only images are allowed.");
+      return;
+    }
+    if (file.size > maxSize) {
+      setImageFileUploadError("File size should not exceed 5 MB.");
+      return;
+    }
+    setImageFile(file);
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    setImageFileUploading(true);
+
+    const formData = new FormData();
+    formData.append("file", imageFile);
+    formData.append("upload_preset", "trial_upload"); // Use your upload preset here
+    console.log(formData);
+
+    try {
+      const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/dbld1potw/image/upload/`, // Replace with your Cloudinary cloud name
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            const { loaded, total } = progressEvent;
+            let percent = Math.floor((loaded * 100) / total); // Calculate progress percentage
+            setImageFileUploadProgress(percent); // Update progress state
+          },
+        }
+      );
+
+      setImageFileUrl(response.data.secure_url); // Get the image URL from Cloudinary response
+      setImageFileUploading(false);
+      setImageFileUploadProgress(null);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      setImageFileUploading(false);
+    }
+  };
+  console.log(imageFileUrl);
   return (
     <div className="max-w-lg mx-auto w-full h-full flex flex-col justify-between gap-3 p-6">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-6">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={4}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+                text: {
+                  fontSize: "13px",
+                  fill: "#fff",
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full object-cover border-4 border-[lightgray]"
+            className={`rounded-full w-full h-full object-cover border-4 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              "opacity-60"
+            }`}
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
         <TextInput
           type="text"
           id="username"
